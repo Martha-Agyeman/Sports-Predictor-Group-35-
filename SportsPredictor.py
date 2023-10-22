ca67ebef8c385ad3ef1bfae5624a9e8de853ed6c
Martha-Agyeman <123462320+Martha-Agyeman@users.noreply.github.com>
2023-10-22 16:57:04 +0000

    Add files via upload

---
+++ SportsPredictor.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+# Initalise the Flask app
+app = Flask(__name__)
+
+
+
+# Loads pre-trained model
+model = load_model('Sports_Predictor')
